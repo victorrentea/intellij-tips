7b935c48dfdd8d78bbfd18af11ca5629c70b9ab4
Victor Rentea <victorrentea@gmail.com>
2022-09-21 23:48:51 +0300

    yolo

--- src/main/java/victor/training/intellij/speed/IntelliJPlay.kt
+++ src/main/java/victor/training/intellij/speed/IntelliJPlay.kt
@@ -1,51 +1,30 @@
-package victor.training.intellij.speed;
+package victor.training.intellij.speed
 
+import victor.training.intellij.support.dirty.Customer
+import victor.training.intellij.support.dirty.Rental
+import java.time.LocalDateTime
+import java.util.stream.Collectors
+import java.util.stream.Stream
 
-import victor.training.intellij.support.dirty.Customer;
-import victor.training.intellij.support.dirty.Rental;
-
-import java.time.LocalDateTime;
-import java.util.List;
-import java.util.Set;
-import java.util.stream.Stream;
-
-import static java.util.stream.Collectors.toList;
-import static java.util.stream.Collectors.toSet;
-
-
-public class IntelliJPlay {
-   public static void main(String[] args) {
-      List<Integer> numbers = Stream.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10).toList();
-      Person person = new Person();
+fun main(args: Array<String>) {
+    val numbers = Stream.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10).toList()
+    val person = Person()
 
 //      log.debug("ast");
-
-      Set<Integer> set = numbers.stream().map(n -> n + 1).collect(toSet());
-
-
-      person.someNewMethod(numbers);
-      LocalDateTime now;
-      timeBased(person.getCreateTime());
-
-      Customer customer = new Customer();
-      List<Integer> list = customer.getRentals().stream().map(Rental::getDaysRented).collect(toList());
-      System.out.println(list);
-      method(customer.getRentals());
-   }
-
-
-
-
-
-   public static void method(List<Rental> rentals) {
-      rentals.clear();
-   }
-
-   public static void timeBased(LocalDateTime dateTime) {
-      System.out.println("Logic with " + dateTime);
-      System.out.println("asdsad");
-   }
-
-
+    val set = numbers.map { it + 1 }
+    person.someNewMethod(numbers)
+    var now: LocalDateTime
+    val customer = Customer()
+    val list = customer.rentals.stream().map { obj: Rental -> obj.daysRented }.collect(Collectors.toList())
+    println(list)
+    method(customer.rentals)
 }
 
+fun method(rentals: MutableList<Rental?>) {
+    rentals.clear()
+}
+
+fun timeBased(dateTime: LocalDateTime) {
+    println("Logic with $dateTime")
+    println("asdsad")
+}
